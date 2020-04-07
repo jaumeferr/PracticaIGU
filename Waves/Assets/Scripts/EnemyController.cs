2c4f1df0fa4798ae1db415039e75d7d25460b826
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-04-07 19:06:15 +0100

    Añadir proyecto practicas

---
+++ Waves/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Rigidbody player;
+    Rigidbody rb;
+
+    Vector3 moveAmount;
+    Vector3 smoothMoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = this.GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 moveDir = new Vector3(
+            player.position.x - rb.position.x,
+            player.position.y - rb.position.y,
+            player.position.z - rb.position.z).normalized;
+        
+        
+    }
+}
---
+++ Waves/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 8f;
+    public bool paperFound = false;
+    public int score = 0;
+
+    Vector3 moveAmount;
+    Vector3 smoothMoveSpeed;
+
+    enum PlayerState
+    {
+        Walk, Attack, Jump
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 targetMoveAmount = moveDir * playerSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveSpeed, .15f);
+    }
+
+    void addPoints(int points)
+    {
+        this.score += points;
+    }
+
+    void FixedUpdate(){
+        this.GetComponent<Rigidbody>().MovePosition(this.GetComponent<Rigidbody>().position + this.GetComponent<Transform>().TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+
+    void Jump(){
+        
+    }
+}
