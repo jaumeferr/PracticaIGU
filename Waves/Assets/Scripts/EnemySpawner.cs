1d7c1dc1062a5d4f2c95e98b379ee19553d27798
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-04-20 18:36:46 +0100

    Spawn enemigos (1)
    Crear script EnemySpawner y añado objeto vacío "Spawner" a la escena

--- Waves/Assets/Scripts/EnemySpawner.cs
+++ Waves/Assets/Scripts/EnemySpawner.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public int count;
+
+    public Transform prefabEnemy;
+    public Rigidbody planet;
+    public Rigidbody player;
+
+    private Vector3 planetCenter;
+    private float radius;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        radius = 40.0f;
+        count = 15;
+        SpawnEnemies(count);
     }
 
     // Update is called once per frame
@@ -15,4 +26,36 @@ public class EnemySpawner : MonoBehaviour
     {
         
     }
+
+    void SpawnEnemies(int count){
+        int i = count;
+
+        //Spawnear enemigos
+        while(i > 0){
+            //Generar angulo alfa/beta aleatorio
+            float beta = Random.Range(0.0f, 2 * Mathf.PI);
+            float alfa = Random.Range(0.0f, 2 * Mathf.PI);
+
+            //Determinar x,y,z 
+            Vector3 newPos = new Vector3(planet.x + radius*Math.cos(beta), planet.y + radius*Math.sin(beta), planet.z + radius*Math.cos(alfa));
+
+            //Comprobar distancia segura respecto al jugador
+            Vector3 enemyToPlayer = newPos - player.position;
+
+            Debug.Log(
+                enemyToPlayer.magnitude
+            );
+            Debug.Log(
+                newPos
+            );
+
+            if(enemyToPlayer.magnitude > 5){
+                //Generar enemigo
+                Instantiate(prefabEnemy, newPos, Quaternion.identity);
+                //Decrementar contador
+                i --;
+            }
+        }
+    }
+    
 }
