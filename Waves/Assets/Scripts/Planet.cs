c556a1b482bb936b65eecbf5f13c93b24f29ee49
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-05-04 12:42:15 +0100

    Generacion Procedural (1)
    Generacion procedural del terreno del planeta.

---
+++ Waves/Assets/Scripts/Planet.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    //[Range(2, 256)]
+    public int resolution = 100;
+    public bool autoUpdate = true;
+
+    public ShapeSettings shapeSettings;
+    public ColourSettings colourSettings;
+
+    [HideInInspector]
+    public bool shapeSettingsFoldout;
+    [HideInInspector]
+    public bool colourSettingsFoldout;
+
+    ShapeGenerator shapeGenerator;
+
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    private void OnValidate()
+    {
+        GeneratePlanet();
+    }
+
+    void Initialize()
+    {
+        //InitializeShapeSettings();
+        shapeSettings = new ShapeSettings();
+        shapeSettings.InitializeNoiseLayers();
+
+        shapeGenerator = new ShapeGenerator(shapeSettings);
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+            terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    public void GeneratePlanet()
+    {
+        Initialize();
+        GenerateMesh();
+        GenerateColours();
+    }
+
+    public void OnShapeSettingsUpdated()
+    {
+        if (autoUpdate)
+        {
+            Initialize();
+            GenerateMesh();
+        }
+    }
+
+    public void OnColourSettingsUpdated()
+    {
+        if (autoUpdate)
+        {
+            Initialize();
+            GenerateColours();
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+
+        //Add mesh collider
+        GameObject[] planetFaces = GameObject.FindGameObjectsWithTag("PlanetFace");
+        if(planetFaces != null){
+            foreach (GameObject item in planetFaces)
+            {
+             item.AddComponent<MeshCollider>();   
+            }
+        }
+    }
+
+    void GenerateColours()
+    {
+        foreach (MeshFilter m in meshFilters)
+        {
+            Color rdPlanetColor = new Color((float)Random.Range(0,255), (float)Random.Range(0,255), (float)Random.Range(0,255));
+            //m.GetComponent<MeshRenderer>().sharedMaterial.color = rdPlanetColor;
+
+            m.GetComponent<MeshRenderer>().sharedMaterial.color = colourSettings.planetColour;
+        }
+    }
+
+    void InitializeShapeSettings(){
+        //this.shapeSettings = new ShapeSettings();
+    }
+}
---
+++ Waves/Assets/Scripts/ShapeSettings.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ShapeSettings : ScriptableObject
+{
+    public float planetRadius;
+    public NoiseLayer[] noiseLayers;
+    
+    //[System.Serializable]
+    public ShapeSettings(){
+        this.planetRadius = 40;
+        this.noiseLayers = new NoiseLayer[2];
+    }
+
+    public void InitializeNoiseLayers(){
+        //Initialize NoiseLayer 1
+        NoiseSettings noiseSettings1 = new NoiseSettings(
+            (float)Random.Range(0.15f,0.25f), 
+            2, 
+            (float)Random.Range(0.5f, 0.6f), 
+            (float)Random.Range(3.3f, 3.7f), 
+            (float)Random.Range(0.2f, 0.3f), 
+            Vector3.zero, 
+            0);
+
+        //Initialize Noise Layer 2
+        NoiseSettings noiseSettings2 = new NoiseSettings(
+            (float)Random.Range(0.5f,1), 
+            2, 
+            (float)Random.Range(0.4f,1), 
+            2, 
+            (float)Random.Range(0.1f, 0.3f), 
+            Vector3.zero, 
+            0);
+
+        noiseLayers[0] = new NoiseLayer(noiseSettings1, false);
+        noiseLayers[1] = new NoiseLayer(noiseSettings2, true);
+    }
+    public class NoiseLayer{
+        public bool enable = true;
+        public bool useFirstLayerAsMask;
+        public NoiseSettings noiseSettings;
+
+        public NoiseLayer(NoiseSettings noiseSettings, bool useFirstLayerAsMask){
+            this.noiseSettings = noiseSettings;
+            this.useFirstLayerAsMask = useFirstLayerAsMask;
+        }
+    }
+}
