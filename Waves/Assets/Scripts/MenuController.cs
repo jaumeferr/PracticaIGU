ee7dbaf272228eb65861cb51f1f9b2da68dd24fa
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-06-18 16:25:13 +0100

    Skyboxes

--- Waves/Assets/Scripts/MenuController.cs
+++ Waves/Assets/Scripts/MenuController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuController : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameObject LevelsPanel;
+    public GameObject SettingsPanel;
+
+    private void Start() {
         
+    }
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            LevelsPanel.SetActive(false);
+            SettingsPanel.SetActive(false);
+        }
+    }
+    public void OpenLevelsPanel(){
+        LevelsPanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OpenSettingsPanel(){
+        SettingsPanel.SetActive(true);
     }
+
 }
