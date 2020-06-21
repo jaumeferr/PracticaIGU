b2da5212cbdb325ae1d267545b3fe46f67bc3019
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-06-21 23:42:03 +0100

    arreglillos + exe

---
+++ Waves/Assets/Scripts/Menus/VictoryScoreController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryScoreController : MonoBehaviour
+{
+    public Text text;
+    void Start()
+    {
+        if(Variables.currentLevel == 2){
+            GameObject.Find("NextLevelButton").SetActive(false);
+        }
+        text.text = Variables.lastScore.ToString();
+    }
+}
